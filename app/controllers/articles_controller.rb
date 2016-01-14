896c0fd517f204d6e633b7aeb3a2a29f79da2f99
Minh Nguyen <lawandcontradiction@gmail.com>
2016-01-14 13:03:33 +1100

    Restructured forms, add edit and update functions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,4 +26,15 @@ class ArticlesController < ApplicationController
     
     redirect_to articles_url
   end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    
+    redirect_to article_path(@article)
+  end
 end
